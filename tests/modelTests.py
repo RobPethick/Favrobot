2b50cbf9d8aabe7a806060ef3d17bbf761923393
Robert Pethick <robert.pethick@softwaresolved.com>
2016-09-02 12:52:46 +0100

    Add Test widget method

--- tests/modelTests.py
+++ tests/modelTests.py
@@ -1,4 +1,5 @@
 from models.user import User
+from models.widget import Widget
 import unittest
 
 class TestUserMethods(unittest.TestCase):
@@ -15,6 +16,36 @@ class TestUserMethods(unittest.TestCase):
         #Assert
         self.assertEqual(userId, user.userId)
         self.assertEqual(role, user.role)
+
+class TestWidgetMethods(unittest.TestCase):
+
+    def test_constructorParsesDictionary(self):
+        # Arrange
+        widgetType = 'board'
+        name = 'testBoard'
+        color = 'red'
+        publicSharing = 'none'
+        widgetId = '123'
+        userId1 = 'abc123'
+        userId2 = 'xyz789'
+        userDictionary1 = {'userId': userId1, 'role': 'admin'}
+        userDictionary2 = {'userId': userId2, 'role': 'admin'}
+        dictionary = {'type': widgetType, 'name': name, 'color': color, 'publicSharing': publicSharing, 'widgetCommonId': widgetId, 'sharedToUsers': [userDictionary1, userDictionary2]}
+
+        # Act
+        widget = Widget(dictionary)
+
+        # Assert
+        self.assertEqual(widgetType, widget.type)
+        self.assertEqual(name, widget.name)
+        self.assertEqual(color, widget.color)
+        self.assertEqual(publicSharing, widget.publicSharing)
+        self.assertEqual(widgetType, widget.type)
+        self.assertEqual(2, len(widget.users))
+        self.assertEqual(userId1, widget.users[0].userId)
+        self.assertEqual(userId2, widget.users[1].userId)
+
+        
 
 if __name__ == '__main__':
     unittest.main()

a8da68d86a844f2cbd8db1d5c1baf3ce5bb61730
Robert Pethick <robert.pethick@softwaresolved.com>
2016-09-09 22:59:54 +0100

    Finish widget tests

--- tests/modelTests.py
+++ tests/modelTests.py
@@ -1,6 +1,7 @@
 from models.user import User
 from models.widget import Widget
 import unittest
+import datetime
 
 class TestUserMethods(unittest.TestCase):
 
@@ -18,6 +19,19 @@ class TestUserMethods(unittest.TestCase):
         self.assertEqual(role, user.role)
 
 class TestWidgetMethods(unittest.TestCase):
+
+    def setUp(self):
+        widgetType = 'board'
+        name = 'testBoard'
+        color = 'red'
+        publicSharing = 'none'
+        widgetId = '123'
+        userId1 = 'abc123'
+        userId2 = 'xyz789'
+        userDictionary1 = {'userId': userId1, 'role': 'admin'}
+        userDictionary2 = {'userId': userId2, 'role': 'admin'}
+        dictionary = {'type': widgetType, 'name': name, 'color': color, 'publicSharing': publicSharing, 'widgetCommonId': widgetId, 'sharedToUsers': [userDictionary1, userDictionary2]}
+        self.basicWidget = Widget(dictionary)
 
     def test_constructorParsesDictionary(self):
         # Arrange
@@ -45,7 +59,61 @@ class TestWidgetMethods(unittest.TestCase):
         self.assertEqual(userId1, widget.users[0].userId)
         self.assertEqual(userId2, widget.users[1].userId)
 
+    def test_isDailyGoalsBoardReturnsFalseIfNotABoard(self):
+        # Arrange
+        self.basicWidget.type = 'backlog'      
+        self.basicWidget.name = 'Daily Goals 13-01-20'
         
+        # Act
+        isDailyBoard = self.basicWidget.isDailyBoard()
+
+        # Assert
+        self.assertFalse(isDailyBoard)
+
+    def test_isDailyGoalsBoardReturnsFalseIfNameDoesntMatch(self):
+        # Arrange
+        self.basicWidget.type = 'board'     
+        self.basicWidget.name = 'TestBoard' 
+        
+        # Act
+        isDailyBoard = self.basicWidget.isDailyBoard()
+
+        # Assert
+        self.assertFalse(isDailyBoard)
+
+    def test_isDailyGoalsBoardReturnsTrueIfBoardAndNameMatches(self):
+        # Arrange
+        self.basicWidget.type = 'board'     
+        self.basicWidget.name = 'Daily Goals 13-06-15' 
+        
+        # Act
+        isDailyBoard = self.basicWidget.isDailyBoard()
+
+        # Assert
+        self.assertTrue(isDailyBoard)
+
+    def test_returnDateReturnsNullIfNotDailyGoalsBoard(self):
+        # Arrange
+        self.basicWidget.type = 'backlog'     
+        self.basicWidget.name = 'Daily Goals 13-06-15' 
+        
+        # Act
+        isDailyBoard = self.basicWidget.getDate()
+
+        # Assert
+        self.assertIsNone(isDailyBoard)
+
+    def test_correctlyParsesDate(self):
+        # Arrange
+        self.basicWidget.type = 'board'     
+        self.basicWidget.name = 'Daily Goals 13-06-2020' 
+        
+        # Act
+        isDailyBoard = self.basicWidget.getDate()
+
+        # Assert
+        self.assertEqual(isDailyBoard, datetime.datetime(2020, 6, 13))
+
 
 if __name__ == '__main__':
     unittest.main()
